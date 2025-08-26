4055d7a6a98c7a5734e463fbce0d7d30963b9128
Sukhmanpreet Singh <ssbakshi2050@gmail.com>
2025-08-26 11:24:54 +0530

    Add files via upload

---
+++ FD Calculator/script.js
@@ -0,0 +1,21 @@
+function calculateMaturityAmount() {
+
+    // Get input values from the form elements
+
+    const principle = parseFloat(document.getElementById('principle').value);
+    const interestRate = parseFloat(document.getElementById('interestRate').value);
+    const tenure = parseFloat(document.getElementById('tenure').value);
+
+    // Perform the calculations
+
+    const maturityAmount = principle + (principle * interestRate * tenure) / 100;
+
+    // Display Result
+
+    document.getElementById('result').innerText = `Maturity Amount : ${maturityAmount.toFixed(2)}`;
+
+}
+
+// Attach the event listner to the calculate button
+
+document.getElementById("calculateBtn").addEventListener("click", calculateMaturityAmount); 
